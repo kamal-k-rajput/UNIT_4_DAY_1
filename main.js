717d4a24929eb6f747b4f5b7422a65835879839d
kamal-kishor-rajput <90085564+kamal-kishor-rajput@users.noreply.github.com>
2022-03-03 00:49:07 +0530

    require and export assignment

---
+++ main.js
@@ -0,0 +1,9 @@
+let add = require("./add");
+let divide = require("./divide");
+let substract = require("./substract");
+let multiply = require("./multiply");
+
+console.log(add(3, 4));
+console.log(substract(3, 4));
+console.log(multiply(3, 4));
+console.log(divide(3, 4));
